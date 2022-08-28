1afcf2a8b54946dae1bee67b456a1636e540be3e
TNiurhechev <66021340+TNiurhechev@users.noreply.github.com>
2022-08-28 20:38:25 +0300

    UPDATE 2.0.2 - Minor design changes and optimisation - Abstraction in DAO classes implemented

--- src/main/java/com/example/demo1/AppVersion.java
+++ src/main/java/com/example/demo1/AppVersion.java
@@ -2,7 +2,7 @@ package com.example.demo1;
 
 public class AppVersion {
     private static final String appName = "AW Shop Manager";
-    private static final String version = "2.0.1";
+    private static final String version = "2.0.2";
     public static String getCurrentVersion(){
         return appName+" "+version;
     }
